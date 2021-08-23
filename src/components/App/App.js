a7e115497fd91767923addff71464121c3c8ad7a
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-08-23 13:52:30 +0200

    adds files from hw-03

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,24 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
+
   addContact = data => {
     const { name, number } = data;
     const { contacts } = this.state;
